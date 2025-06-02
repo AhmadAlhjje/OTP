a038ceee869eab90bcf50ba61c880e4f69a0efec
obada63 <ahmadobadaal7jje@gmail.com>
2025-06-02 22:54:52 +0300

    Done

--- src/app/dashboard/my_accounts/page.tsx
+++ src/app/dashboard/my_accounts/page.tsx
@@ -1,10 +1,14 @@
-import AccountList from './AccountList'
+"use client";
+import useTranslation from "@/hooks/useTranslation";
+import AccountList from "./AccountList";
 
 export default function AccountsPage() {
+
+  const { t } = useTranslation();
   return (
     <main className="p-4">
-      <h1 className="text-2xl font-bold mb-4">الحسابات المرتبطة</h1>
+      <h1 className="text-2xl font-bold mb-4">{t("linked_accounts")}</h1>{" "}
       <AccountList />
     </main>
-  )
+  );
 }
